8dab18fbd59ffbf0c26ccffdde081f87957aab2d
Jari De Wulf <72047328+jaridewulf@users.noreply.github.com>
2021-09-22 23:18:33 +0200

    Update Github link

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -7,7 +7,7 @@ const Socials = () => {
         <span className="socials">
             <a className="social-link" href="https://www.behance.net/jaridewulf" target="_blank" rel="noreferrer noopener">
                 <img className="social-link-image" src={behance}width="60" height="60" alt="Jari De Wulf - Behance Design Portfolio" /></a>
-            <a className="social-link" href="https://github.com/JariDevine" target="_blank" rel="noreferrer noopener">
+            <a className="social-link" href="https://github.com/jaridewulf" target="_blank" rel="noreferrer noopener">
                 <img className="social-link-image" src={github}width="60" height="60" alt="Jari De Wulf - Github Developer Repository" /></a>
             <a className="social-link" href="https://www.linkedin.com/in/jaridewulf/" target="_blank" rel="noreferrer noopener">
                 <img className="social-link-image" src={linkedin}width="60" height="60" alt="Jari De Wulf - Front-End Developer Linkedin Profile"/></a>
