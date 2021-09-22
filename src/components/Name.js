5ce969ee6402bcbf981a5502b828723134f131fb
Jari De Wulf <jari.de.wulf@student.howest.be>
2021-09-22 20:10:58 +0200

    Update steps

--- src/components/Name.js
+++ src/components/Name.js
@@ -1,23 +1,19 @@
-import Typical from 'react-typical'
-
-    const strings=['velop', 'sign'];
-    const typeSpeed=40;
-
-
+import Typical from "react-typical";
 const Name = () => {
-    return ( <h1 className="name">Jari<br/> De<span className="typedText"><Typical 
-        loop={Infinity}
-        wrapper='b' 
-        steps={[
-          'veloper',
-          2000,
-          'signer',
-          2000,
-          'inzenaar',
-          2000,
-          'biel',
-          2000
-        ]}/></span><br/> Wulf</h1> );
-}
- 
+  return (
+    <h1 className="name">
+      Jari
+      <br /> De
+      <span className="typedText">
+        <Typical
+          loop={Infinity}
+          wrapper="b"
+          steps={["veloper", 2000, "signer", 2000, "inzenaar", 2000]}
+        />
+      </span>
+      <br /> Wulf
+    </h1>
+  );
+};
+
 export default Name;
